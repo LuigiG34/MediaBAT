fb891aac820beefc82ba02e07577015740bec091
LuigiG34 <luigigandemer6@gmail.com>
2022-08-22 13:02:35 +0200

    App Start File

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// use Autololoader & Router to start the App
+use App\Core\Router;
+use App\Autoloader;
+
+// Define URL at project base
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']));
+
+// Get Autoloader
+require_once './Autoloader.php';
+Autoloader::register();
+
+// instantiate Router
+$app = new Router();
+
+// Start the app
+$app->start();
