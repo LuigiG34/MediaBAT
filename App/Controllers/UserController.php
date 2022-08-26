ad3ff1283a5e7ed9ab3f5e381c742f2020037f3c
LuigiG34 <luigigandemer6@gmail.com>
2022-08-26 09:31:09 +0200

    User Controller

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController
 
     /**
      * sets user basic data
-     * On hydrate (deplacer dans controller)
+     * hydrate & set session
      *
      * @param $id
      * @param $fname
@@ -49,16 +49,69 @@ class UserController
         $this->UserClass->setSession();
     }
 
+    /**
+     * signUpUser function
+     * 
+     * Register controller
+     */
     public function signUpUser()
     {
+        if(isset($_POST['submit'])){
+            $fname = htmlspecialchars($_POST['fname']);
+            $lname = htmlspecialchars($_POST['lname']);
+            $email = htmlspecialchars($_POST['email']);
+            $pass = htmlspecialchars($_POST['password']);
+            $city = htmlspecialchars($_POST['city']);
+            $zipcode = htmlspecialchars($_POST['zipcode']);
+            $birthdate = htmlspecialchars($_POST['date']);
+            $status = htmlspecialchars($_POST['status']);
+            if(isset($_POST['metier'])){
+                $job = htmlspecialchars($_POST['metier']);
+            }else{
+                $job = "null";
+            }
+            $password = password_hash($pass, PASSWORD_ARGON2I);
+            $this->UserManager->signUp($fname,$lname,$email,$password,$birthdate,$status,$city,$zipcode,$job);
 
+            CoreController::alert("success", "You successfully registered.");
+            header('Location: home');
+        }else{
+            CoreController::alert("danger", "An error occured.");
+            header('Location: signup');
+        }
     }
 
+    /**
+     * signInUser function
+     *
+     * gets data and compares it to inputs
+     */
     public function signInUser()
     {
+        if(isset($_POST['submit'])){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
 
+            $data = $this->UserManager->getByEmail($email);
+
+            if($data){
+                $this->setUserBasic($data->id_user,$data->fname_user,$data->lname_user,$data->email_user,$data->password_user,$data->birthdate_user,$data->status_user,$data->registration_date_user,$data->city_user,$data->zipcode_user,$data->job_user);
+
+                if(password_verify($password, $this->UserClass->getPassword_user())) {
+                    CoreController::alert("success","User logged in successfully !");
+                    header("Location: home");
+                }else{
+                    CoreController::alert("danger","Passwords don't match !");
+                }
+            }else{
+                CoreController::alert("danger","User not found !");
+            }
+        }
     }
 
+    /**
+     * signOutUser function
+     */
     public function signOutUser()
     {
         if(!isset($_SESSION['user'])){
@@ -67,6 +120,6 @@ class UserController
 
         session_destroy();
         unset($_SESSION['user']);
-        header("Location: accueil");
+        header("Location: home");
     }
 }
